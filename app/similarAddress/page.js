6f1366ee637de6c67558440ca1f14751c59967b6
harshAswani1109 <ha1109@srmist.edu.in>
2024-09-25 10:29:53 +0530

    commit for trigger

--- app/similarAddress/page.js
+++ app/similarAddress/page.js
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 // import Link from "next/link";
 import { MOCK_ADDRESS_DATA } from "@/constants";
